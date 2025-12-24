ffb8e7d46a7df2e67e937baede466b77be670a65
zizicici <1940699+zizicici@users.noreply.github.com>
2025-12-24 12:39:23 +0800

    [chore] Bump Version: 1.6.11 -> 1.6.12

--- Off Day/More/SpecificationsViewController.swift
+++ Off Day/More/SpecificationsViewController.swift
@@ -73,7 +73,7 @@ class SpecificationsViewController: UIViewController {
                 case .publisher:
                     return "ZIZICICI LIMITED"
                 case .date:
-                    return "2025/11/06"
+                    return "2025/12/24"
                 case .license:
                     return "闽ICP备2023015823号-8A"
                 }
